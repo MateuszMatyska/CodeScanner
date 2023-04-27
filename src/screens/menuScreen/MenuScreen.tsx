27dc4f575812e0ffbe8d6d24e62bcb83bff4fb11
Mateusz Matyska <mateusz.matyska_extern@bsdex.de>
2023-04-27 17:48:44 +0200

    feat: add menu screen

--- src/screens/menuScreen/MenuScreen.tsx
+++ src/screens/menuScreen/MenuScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Container from '../../components/container/Container';
+import Button from '../../components/button/Button';
+import {navigate} from '../../navigation/services/NavigationService';
+import {ScreenNames} from '../../navigation/const/ScreensNames';
 
 const MenuScreen: React.FC = () => {
   return (
-    <View>
-      <Text>Menu</Text>
-    </View>
+    <Container>
+      <Button onPress={() => navigate(ScreenNames.Camera)} title="Camera" />
+    </Container>
   );
 };
 
